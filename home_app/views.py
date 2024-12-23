c3994a35942604cc6e614956af67e050fb751392
DmytroLomako <lomakodmytro@gmail.com>
2024-12-23 23:08:23 +0100

    added the ability to select color and memory for the product

--- home_app/views.py
+++ home_app/views.py
@@ -6,6 +6,19 @@ def render_home():
     list_products = Product.query.all()
     return flask.render_template('catalog.html', list_products = list_products)
 
-def render_product(product_id):
+def render_product(product_id, product_color, product_memory):
+    list_colors = ['black', 'violet', 'midnightblue', 'darkblue', 'gold', 'green', 'red']
+    list_memory = ['64GB', '128GB', '256GB', '512GB', '1TB']
     product = Product.query.get(product_id)
-    return flask.render_template('product.html', product = product)
+    color_index = list_colors.index(product_color)
+    memory_index = list_memory.index(product_memory)
+    list_colors[color_index] += ' active'
+    list_memory[memory_index] += ' active'
+    if flask.request.method == 'POST':
+        if flask.request.form.get('memory_button'):
+            product_memory = flask.request.form.get('memory_button')
+        if flask.request.form.get('color_button'):
+            product_color = flask.request.form.get('color_button')
+        print(1)
+        return flask.redirect(f'/product/{product_id}{product_color}&{product_memory}')
+    return flask.render_template('product.html', product = product, colors = list_colors, memories = list_memory, active_color = product_color, active_memory = product_memory)
--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,7 @@ import home_app, auth_app
 from .settings import project
 
 home_app.home_app.add_url_rule('/', view_func = home_app.render_home, methods = ['GET', 'POST'])
-home_app.home_app.add_url_rule('/product/<int:product_id>', view_func = home_app.render_product)
+home_app.home_app.add_url_rule('/product/<int:product_id><string:product_color>&<string:product_memory>', view_func = home_app.render_product, methods = ['GET', 'POST'])
 auth_app.auth_app.add_url_rule('/auth', view_func = auth_app.render_auth, methods = ['GET', 'POST'])
 
 project.register_blueprint(home_app.home_app)
