0ffa68bf56b95ff7aa5ae774afcf64fa90d4cbb8
DmytroLomako <lomakodmytro@gmail.com>
2025-02-03 11:41:52 +0200

    improved filter by price

--- home_app/static/js/catalog.js
+++ home_app/static/js/catalog.js
@@ -6,47 +6,10 @@ let filterMinPrice = document.querySelector('.min-price-filter');
 let priceFilterButton = document.querySelector('.price-filter-button');
 let maxPriceAll = Number(document.querySelector('.max-all').value)
 let minPriceAll = Number(document.querySelector('.min-all').value)
-
-filterMaxPrice.addEventListener('input', (event) => {
-    if (filterMaxPrice.value > Number(filterMaxPrice.id) || filterMaxPrice.value < Number(filterMinPrice.id)){
-        priceFilterButton.disabled = true;
-        filterMaxPrice.style.color = '#be0202';
-    } else {
-        priceFilterButton.disabled = false;
-        filterMaxPrice.style.color = 'black';
-    }
-})
-
-filterMinPrice.addEventListener('input', (event) => {
-    if (filterMinPrice.value > Number(filterMaxPrice.id) || filterMinPrice.value < Number(filterMinPrice.id)){
-        priceFilterButton.disabled = true;
-        filterMinPrice.style.color = '#be0202';
-    } else {
-        priceFilterButton.disabled = false;
-        filterMinPrice.style.color = 'black';
-    }
-})
-
-filterButton.forEach(function (button) {
-    button.addEventListener('click', function (event){
-        let input = button.querySelector('input')
-        if (event.target !== input){
-            input.checked = !input.checked;
-        }
-    })
-})
-
-filterCheckbox.forEach(function (checkbox) {
-    checkbox.addEventListener('change', function (){
-        console.log(checkbox)
-        filterForm.submit();
-    })
-})
-
 const priceSlider = document.getElementById('price-range');
 const minPrice = Number(priceSlider.dataset.minPrice);
 const maxPrice = Number(priceSlider.dataset.maxPrice);
-console.log(minPrice, maxPrice)
+
 
 noUiSlider.create(priceSlider, {
     start: [minPrice, maxPrice], 
@@ -71,3 +34,43 @@ priceSlider.noUiSlider.on('update', function (values, handle) {
         filterMaxPrice.value = values[1];
     }
 });
+
+filterMaxPrice.addEventListener('input', (event) => {
+    if (Number(filterMaxPrice.value) < Number(filterMinPrice.value) || Number(filterMaxPrice.value) > Number(filterMaxPrice.id)){
+        priceFilterButton.disabled = true;
+        filterMaxPrice.style.color = '#be0202';
+    } else {
+        priceFilterButton.disabled = false;
+        filterMaxPrice.style.color = 'black';
+        filterMinPrice.style.color = 'black';
+        priceSlider.noUiSlider.set([null, Number(filterMaxPrice.value)]);
+    }
+})
+
+filterMinPrice.addEventListener('input', (event) => {
+    if (Number(filterMinPrice.value) > Number(filterMaxPrice.value) || Number(filterMinPrice.value) < Number(filterMinPrice.id)){
+        priceFilterButton.disabled = true;
+        filterMinPrice.style.color = '#be0202';
+    } else {
+        priceFilterButton.disabled = false;
+        filterMinPrice.style.color = 'black';
+        filterMaxPrice.style.color = 'black';
+        priceSlider.noUiSlider.set([Number(filterMinPrice.value), null]);
+    }
+})
+
+filterButton.forEach(function (button) {
+    button.addEventListener('click', function (event){
+        let input = button.querySelector('input')
+        if (event.target !== input){
+            input.checked = !input.checked;
+        }
+    })
+})
+
+filterCheckbox.forEach(function (checkbox) {
+    checkbox.addEventListener('change', function (){
+        console.log(checkbox)
+        filterForm.submit();
+    })
+})
