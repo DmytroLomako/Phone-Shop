29587951279f028e9b7fe5b89300e1fbb3cd36ea
DmytroLomako <lomakodmytro@gmail.com>
2025-02-10 14:48:22 +0200

    added the ability to change the main product on the admin panel

--- admin_app/static/js/admin.js
+++ admin_app/static/js/admin.js
@@ -1,28 +1,33 @@
-let memoryInput = document.querySelector('.new-product-input-memory input')
-let dropdownMenuButton = document.getElementById('dropdown-menu-button')
+let memoryInput = document.querySelector('.new-product-input-memory input');
+let dropdownMenuButton = document.querySelector('dropdown-menu-button');
 let dropdownMenu = document.querySelector('.dropdown-menu');
-let dropdownButtons = dropdownMenu.querySelectorAll('button')
-let addNewProductText = document.querySelector('.add-new-product-text')
-let cancelAddNewProduct = document.querySelector('.cancel-new-product')
-let overlayAddNewProduct = document.querySelector('.new-product-overlay')
+let dropdownButtons = dropdownMenu.querySelectorAll('button');
+let addNewProductText = document.querySelector('.add-new-product-text');
+let cancelAddNewProduct = document.querySelector('.cancel-new-product');
+let overlayProduct = document.querySelector('.product-overlay');
+let newProductDiv = document.querySelector('.new-product-div');
 
 addNewProductText.addEventListener('click', () => {
-    overlayAddNewProduct.style.display = 'flex';
+    overlayProduct.style.display = 'flex';
+    newProductDiv.style.display = 'flex';
 })
 
 cancelAddNewProduct.addEventListener('click', () => {
-    overlayAddNewProduct.style.display = 'none';
+    overlayProduct.style.display = 'none';
+    newProductDiv.style.display = 'none';
 })
 
-dropdownMenuButton.addEventListener('click', () => {
-    if (dropdownMenu.style.display === 'flex'){
-        dropdownMenu.style.display = 'none';
-        dropdownMenuButton.style.transform = 'rotate(0)';
-    } else{
-        dropdownMenu.style.display = 'flex';
-        dropdownMenuButton.style.transform = 'rotate(180deg)';
-    }
-})
+if (dropdownMenuButton){
+    dropdownMenuButton.addEventListener('click', () => {
+        if (dropdownMenu.style.display === 'flex'){
+            dropdownMenu.style.display = 'none';
+            dropdownMenuButton.style.transform = 'rotate(0)';
+        } else{
+            dropdownMenu.style.display = 'flex';
+            dropdownMenuButton.style.transform = 'rotate(180deg)';
+        }
+    })
+}
 
 dropdownButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -30,3 +35,61 @@ dropdownButtons.forEach(button => {
         dropdownMenu.style.display = 'none';
     })
 })
+
+let buttonEditList = document.querySelectorAll('.button-edit')
+let chooseEditDiv = document.querySelector('.choose-edit')
+let cancelChooseEdit = document.querySelector('.cancel-choose-edit')
+let editMainProductText = document.querySelector('.edit-main-product-text')
+let editMainProductForm = document.querySelector('.edit-main-product-form')
+let editProductDiversityText = document.querySelector('.edit-diversity-product-text')
+let addProductDiversityText = document.querySelector('.add-diversity-product-text')
+
+buttonEditList.forEach(button => {
+    button.addEventListener('click', () => {
+        overlayProduct.style.display = 'flex';
+        chooseEditDiv.style.display = 'flex';
+        chooseEditDiv.querySelector('h1').textContent = button.value;
+        chooseEditDiv.id = button.id;
+    })
+})
+
+cancelChooseEdit.addEventListener('click', () => {
+    chooseEditDiv.style.display = 'none';
+    overlayProduct.style.display = 'none';
+})
+
+editMainProductText.addEventListener('click', () => {
+    chooseEditDiv.style.display = 'none';
+    editMainProductForm.querySelector('input').value = chooseEditDiv.id;
+    editMainProductForm.submit();
+})
+
+let editMainProduct = document.querySelector('.edit-main-product')
+let cancelEditMainProduct = document.querySelector('.cancel-edit-main')
+let memoryInput2 = document.querySelector('.edit-product-input-memory input');
+let dropdownMenuButton2 = document.querySelector('.dropdown-menu-button2');
+let dropdownMenu2 = document.querySelector('.dropdown-menu2');
+
+if (editMainProduct){
+    let dropdownButtons2 = dropdownMenu2.querySelectorAll('button');
+    cancelEditMainProduct.addEventListener('click', () => {
+        editMainProduct.style.display = 'none';
+        overlayProduct.style.display = 'none';
+    })
+    dropdownMenuButton2.addEventListener('click', () => {
+        if (dropdownMenu2.style.display === 'flex'){
+            dropdownMenu2.style.display = 'none';
+            dropdownMenuButton2.style.transform = 'rotate(0)';
+        } else{
+            dropdownMenu2.style.display = 'flex';
+            dropdownMenuButton2.style.transform = 'rotate(180deg)';
+        }
+    })
+    
+    dropdownButtons2.forEach(button => {
+        button.addEventListener('click', () => {
+            memoryInput2.value = button.textContent;
+            dropdownMenu2.style.display = 'none';
+        })
+    })
+}
--- admin_app/views.py
+++ admin_app/views.py
@@ -8,6 +8,7 @@ from flask_login import current_user
 def render_admin():
     if current_user.is_authenticated and current_user.is_admin:
         all_products = Product.query.all()
+        edit_product = None
         list_colors = []
         list_memories = []
         list_images = []
@@ -38,17 +39,60 @@ def render_admin():
                 product_diversity = ProductDiversity(price=price, color=color, memory=memory, description=description, image=image_path, product=product)
                 database.session.add(product_diversity)
                 database.session.commit()
+            elif 'edit_product_id' in flask.request.form:
+                product_id = flask.request.form.get('edit_product_id')
+                edit_product = Product.query.get(product_id)
             # elif 'edit_product_form' in flask.request.form:
             #     product_id = flask.request.form.get('product_id')
             #     product = Product.query.get(product_id)
+            elif 'edit_main_product_form' in flask.request.form:
+                product_id = flask.request.form.get('edit_main_product_form')
+                product = Product.query.get(product_id)
+                name = flask.request.form.get('name')
+                image = flask.request.files['image']
+                brand = flask.request.form.get('brand')
+                price = flask.request.form.get('price')
+                color = flask.request.form.get('color')
+                memory = flask.request.form.get('memory')
+                description = flask.request.form.get('description')
+                
+                image_folder = name.lower().replace(' ', '-')
+                image_path = f'{image_folder}/{color}.webp'
+                image_full_path = os.path.abspath(__file__ + f'/../../home_app/static/img/phones/{image_path}')
+                
+                image_old_path = product.image
+                image_old_full_path = os.path.abspath(__file__ + f'/../../home_app/static/img/phones/{image_old_path}')
+                
+                try:
+                    os.rename(image_old_full_path, image_full_path)
+                except:
+                    image_folder_path = os.path.abspath(__file__ + f'/../../home_app/static/img/phones/{image_folder}')
+                    image_old_folder_path = os.path.abspath(__file__ + f'/../../home_app/static/img/phones/{image_old_path.split('/')[0]}')
+                    os.rename(image_old_folder_path, image_folder_path)
+                    os.rename(f'{image_folder_path}/{image_old_path.split('/')[1]}', image_full_path)
+                    
+                if image:
+                    image.save(image_full_path)
+                
+                product.name = name
+                product.image = image_path
+                product.brand = brand
+                product.price = price
+                product.color = color
+                product.memory = memory
+                product.description = description
+                
+                database.session.commit()
             else:
                 product_id = flask.request.form.get('product_id')
                 product = Product.query.get(product_id)
                 image_folder = product.name.lower().replace(' ', '-')
                 image_path = f'{image_folder}/{product.color}.webp'
                 image_full_path = os.path.abspath(__file__ + f'/../../home_app/static/img/phones/{image_path}')
+                image_folder_path = os.path.abspath(__file__ + f'/../../home_app/static/img/phones/{image_folder}')
                 os.remove(image_full_path)
+                os.remove(image_folder_path)
                 database.session.delete(product)
                 database.session.commit()
-        return flask.render_template('admin.html', all_products=all_products, list_colors=list_colors, list_memories=list_memories, list_images=list_images, account=current_user.is_authenticated, user=current_user, list_product_cart=list_product_cart, summary_price=summary_price)
+        return flask.render_template('admin.html', all_products=all_products, list_colors=list_colors, list_memories=list_memories, list_images=list_images, account=current_user.is_authenticated, user=current_user, list_product_cart=list_product_cart, summary_price=summary_price, edit_product=edit_product)
     flask.abort(404)
